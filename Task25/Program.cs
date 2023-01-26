ffce966c0b72c82c8f40b397e1fd29910222fb4b
Slava <mistik99@mail.ru>
2023-01-26 17:09:33 +0200

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число!");
+        }
+    }
+
+    return result;
+}
+int numberA = GetNumber("Введите число:");
+int numberB = GetNumber("Введите число возведения в степень:");
+int MultipleNumbers(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.WriteLine(MultipleNumbers(numberA, numberB));
